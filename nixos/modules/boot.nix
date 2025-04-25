5991186f758e73e04ce1fcac4186efbca153b54a
Dean Minnie <dminnie33@gmail.com>
2025-04-25 17:01:26 +0200

    Update boot.nix

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,5 @@
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 }
