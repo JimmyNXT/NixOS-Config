be2f6879cf90deb17ab69c5cfe98f8d5dda8eb7c
Dean Minnie <dminnie33@gmail.com>
2025-04-25 15:16:04 +0200

    Update default.nix

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -5,13 +5,13 @@
     ./boot.nix
     ./env.nix
     ./home-manager.nix
-    ./hyprland.nix
+    #./hyprland.nix
     ./mime.nix
     ./net.nix
     ./nh.nix
     ./nix.nix
     ./timezone.nix
     ./user.nix
-    ./zram.nix
+    #./zram.nix
   ];
 }
