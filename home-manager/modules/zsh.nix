260cf5c02c3393d287c0423a1b0cfd20024f1cbe
Dean Minnie <dminnie33@gmail.com>
2025-04-26 18:41:37 +0200

    Update zsh.nix

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,17 +1,5 @@
 { config, ... }: {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases =
-      let
-        flakeDir = "~/flake";
-      in {
-        ".." = "cd ..";
-      };
-
-    history.size = 10000;
   };
 }
