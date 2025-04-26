715402996690ffb95440c720a17ff064c2f5471e
Dean Minnie <dminnie33@gmail.com>
2025-04-26 12:03:19 +0200

    IDK

--- nixos/modules/i3.nix
+++ nixos/modules/i3.nix
@@ -30,12 +30,12 @@
         };
         displayManager.auto = { 
             enable = true; 
-            user = ${user}; 
+            user = "${user}"; 
             lightdm = {
                 enable = true;
                 greeter.enable = false;
                 autoLogin.enable = true;
-                autoLogin.user = ${user};
+                autoLogin.user = "${user}";
             };
         };
     };
